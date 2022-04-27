82d3585dc323d081163b56fbb97462008783f4ee
Furuya <mitsuyuki.furuya@epkotsoftware.co.jp>
2022-04-27 16:20:39 +0900

    create account lock

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\LoginFormRequest;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use App\Models\User;
 
 
 class AuthController extends Controller
@@ -26,11 +27,45 @@ class AuthController extends Controller
     {
         $credentials = $request->only('email', 'password');
 
-        // ログイン成功の場合
-        if (Auth::attempt($credentials)) {
-            $request->session()->regenerate();
+        // アカウントがロックされていたら弾く
+        $user = User::where('email', '=',  $credentials['email'])->first();
 
-            return redirect()->route('home')->with('success', 'ログイン成功しました！');
+        // 取得したユーザーが存在した場合
+        if (!is_null($user)) {
+            // アカウントロックされている場合
+            if ($user->locked_flg === 1) {
+                // アカウントロックのメッセージ
+                return back()->withErrors([
+                    'danger' => 'アカウントがロックされています。'
+                ]);
+            }
+            // アカウントロックされていない場合 (ログイン処理)
+            if (Auth::attempt($credentials)) {
+                $request->session()->regenerate();
+                // ログイン成功時、エラーカウントが0以外なら初期化
+                if ($user->error_count > 0) {
+                    $user->error_count = 0;
+                    // エラーカウントを保存
+                    $user->save();
+                }
+                // ログイン成功時のメッセージ
+                return redirect()->route('home')->with('success', 'ログイン成功しました！');
+            }
+
+            // ログイン失敗の場合、エラーカウントをインクリメント
+            $user->error_count = $user->error_count + 1;
+            // エラーカウントが5以上の場合、アカウントをロック
+            if ($user->error_count > 5) {
+                $user->locked_flg = 1;
+                // エラーカウントを保存
+                $user->save();
+                // アカウントロックした場合
+                return back()->withErrors([
+                    'danger' => 'アカウントがロックされました！解除したい場合は運営者に連絡してください。'
+                ]);
+            }
+            // エラーカウントを保存
+            $user->save();
         }
 
         // ログインエラーの場合
